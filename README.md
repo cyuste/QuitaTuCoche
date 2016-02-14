d85cd622bfa4331b12e421074bb08700a295d65c
cyuste <c.yuste@yustplayit.com>
2016-02-14 21:01:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+qtpc
+====
+
+A Symfony project created on February 13, 2016, 4:42 pm.
---
+++ src/AppBundle/EventListener/UserCreatedListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Listener responsible to generate QR code when new user persisted
+ */
+/*
+class UserCreatedListener implements EventSubscriberInterface
+{
+
+    private $router;
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            FOSUserEvents::RESETTING_RESET_SUCCESS => 'onPasswordResettingSuccess',
+        );
+    }
+
+    public function onPasswordResettingSuccess(FormEvent $event)
+    {
+        $url = $this->router->generate('homepage');
+
+        $event->setResponse(new RedirectResponse($url));
+    }
+}
+ 
+ */
+
